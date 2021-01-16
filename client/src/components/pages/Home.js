eece836b96c07f177ab9988f5ebad45af4ec57fe
Jessica <jessica@DESKTOP-OF60KE0.localdomain>
2021-01-16 10:46:28 -0800

    doggo walko

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -21,11 +21,13 @@ class Home extends Component {
           <button className = "reflect-button">reflect</button>
           <p id="date">01/15/2021</p>
         </div>
+        <div className= "container"> </div>
 
         <svg viewBox="0 0 200 50" id = "ellipse-viewbox" xmlns="http://www.w3.org/2000/svg">
           <ellipse id="home-ellipse" cx="100" cy="50" rx="150" ry="20"/>
         </svg>
-        <img src={doggo} className="pet-image"/>
+        
+        
       
         
       </div>
