37e59adbb120651acdbd78835291e5097e14495d
Sarah Wang <sarahwang4876@gmail.com>
2021-01-16 16:12:19 -0800

    text editor

--- client/src/components/pages/NewEntry.js
+++ client/src/components/pages/NewEntry.js
@@ -1,25 +1,64 @@
-import React, { Component } from "react";
-import { Editor , EditorState} from 'draft-js';
+import React from "react";
+import ReactDOM from 'react-dom';
+import {Editor, EditorState} from 'draft-js';
+import 'draft-js/dist/Draft.css';
+ 
 import "./NewEntry.css";
-
-class NewEntry extends Component {
+ 
+class NewEntry extends React.Component {
     constructor(props) {
       super(props);
-      // Initialize Default State
-      this.state = {};
+      this.state = {editorState: EditorState.createEmpty()};
+ 
+      this.onChange = (editorState) => this.setState({editorState});
   }
-
+ 
   componentDidMount() {
     // remember -- api calls go here!
   }
-
+ 
   render() {
     return (
-      <>
-      <div>Journaling page</div>
-      </>
-    );
-  }
+    <div >
+        <div>
+          {/* <Toolbar
+            editorState={this.state.editorState}
+            setInlineStyle={this.setInlineStyle}
+            fontFamilyStyleMap={fontFamilyStyleMap}
+            fontSizeStyleMap={fontSizeStyleMap}
+            textColorStyleMap={textColorStyleMap}
+            highlightStyleMap={highlightStyleMap}
+            toggleInlineStyle={this.toggleInlineStyle}
+            toggleBlockType={this.toggleBlockType}
+          /> */}
+          <div>
+            <Editor
+              editorState={this.state.editorState}
+              onChange={this.onChange}
+              // customStyleMap={customStyleMap}
+              handleKeyCommand={this.handleKeyCommand}
+              keyBindingFn={this.mapKeyBindings}
+              // blockRenderMap={extendedBlockRenderMap}
+              // blockStyleFn={(block) => this.getBlockStyle(block, customStyleMap)}
+              placeholder="How was your day?"
+            />
+          </div>
+        </div>
+        <div >
+          <span >
+            {this.state.isSaved ? "All changes saved" : "Unsaved"}
+          </span>
+        </div>
+      </div>
+  //   return (
+  //     <>
+  //     <div>Journaling page</div>
+  //     <Editor editorState={this.state.editorState} onChange={this.onChange}>hello<Editor/>
+  //     </>
+  //   );
+  // }
+  );
+    }
 }
-
+ 
 export default NewEntry;
