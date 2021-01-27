0df8f7ee40aac4a8fefbf638a18c96d0ad39e369
Jessica <jessica@DESKTOP-OF60KE0.localdomain>
2021-01-27 00:05:05 -0800

    last?

--- client/src/components/modules/Question.js
+++ client/src/components/modules/Question.js
@@ -27,15 +27,15 @@ class Navbar extends Component{
         <img src={questionblue} className="question" onClick={this.handleOpenModal}/>  
           <Modal isOpen={this.state.showModal} className="Modalq" overlayClassName="Overlay"> 
             <button className="modal-close-button" onClick={this.handleCloseModal}>X</button>
-            <p className="questions-content">
-              <p className="about-title">instructions</p>
+            <div className="questions-content">
+              <p className="question-title">instructions</p>
               <p className= "questiontext">✩ click reflect to start/update today's entry </p>
               <p className= "questiontext">✩ hearts show your pet's mood</p>
               <p className= "questiontext">✩ journal for at least 2 days in a row to maximize happiness</p>
               <p className= "questiontext">✩ check pet profile to learn more about your pet</p>
               <p className= "questiontext">✩ view past entries in timeline</p>
               <p className= "questiontext">✩ click switch pet to adopt a new pet</p>
-          </p>
+          </div>
           </Modal>
             </>
         );
